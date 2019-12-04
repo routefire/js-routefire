4b696ad48eb63ff0a8d0eebf6bddf74a12b137ad
Steven Li <steven4354@gmail.com>
2019-12-04 13:14:06 -0800

    update exampes

--- examples/new-client.js
+++ examples/new-client.js
@@ -1,16 +1,35 @@
 const Routefire = require("../index")
 
 // @dev update this line before running tests
-const rfClient = new Routefire("<your-routefire-user>", "<your-routefire-password>")
+const rfClient = new Routefire("<your-routefire-username>", "<your-routefire-password>")
 
 // ensuring you entered your username
 const username = rfClient.getUsername()
 console.log("Your username ", username)
 
 const testRun = async () => {
-  const res = await rfClient.refreshToken()
 
-  console.log("res ", res);
+  // getting balances
+  bals = await rfClient.getBalances("BTC")
+  console.log("BTC BALANCE: ", bals)
+
+  // submitting an order
+  algoParams = {
+    "target_seconds": "100",
+    "backfill":       "1.0",
+    "aggression":     "0.0",
+  }
+  
+  order = await rfClient.submitOrder("btc", "usd", "0.003", "", "rfxw", algoParams)
+  console.log("ORDER: ", order)
+
+  // get order status
+  status = await rfClient.getOrderStatus(order['order_id'])
+  console.log("ORDER STATUS: ", status)
+
+  // cancel order
+  orderCancellatiton = await rfClient.cancelOrder(order['order_id'])
+  console.log("ORDER CANCELLATION: ", orderCancellatiton)
 };
 
 testRun()
